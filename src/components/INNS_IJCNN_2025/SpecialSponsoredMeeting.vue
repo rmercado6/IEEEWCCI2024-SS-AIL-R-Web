1a1d56701ef3ed8769faa626c3705358447e726d
Ricardo Mercado <ricardo.97.mc@gmail.com>
2025-01-19 11:29:07 +0000

    feat(news-section): update content

--- src/components/INNS_IJCNN_2025/SpecialSponsoredMeeting.vue
+++ src/components/INNS_IJCNN_2025/SpecialSponsoredMeeting.vue
@@ -5,17 +5,108 @@
     <!-- Special Sponsored Meeting -->
     <div class="section pt-0">
         <h3 class="text-3xl font-bold flex flex-row gap-3 mb-5">
-            News: AI Policy Workshop @IJCNN 2025
+            News: AI Policy and Regulation Workshop @IJCNN 2025
         </h3>
         <p>
             This year, apart from our Special Session on ‘AI, Law and Regulation’, we are pleased to announce that we
-            will be hosting a workshop on AI Policy: Today and Tomorrow intended to support our technical IJCNN
+            will be hosting a workshop on AI Policy and Regulation intended to support our technical IJCNN
             community to navigate the imminent AI regulatory landscape. We will have an opportunity to hear from leading
             figures in the European as well as International AI Policy field, particularly in relation to the
             enforcement of the AI Act.<br/>
-            More details on the speakers and the date will be announced soon.
+            More details on the registration and the date will be released <b>very soon</b>. 
         </p>
-        <img src="/rome.png" alt="Call for papers. AI, Law and regulations special session. IJCNN"/>
+        <img src="/rome2.png" alt="Call for papers. AI, Law and regulations special session. IJCNN"/>
+        <div class="flex justify-center p-3">
+            <span class="border-2 rounded-md p-3 border-secondary-600 bg-secondary-600/10">
+                <b>Date and Registration details:</b> to be anounced
+            </span>
+        </div>
+        <p>
+            This AI Policy and Regulation workshop responds to the imminent global AI policy and regulatory changes with
+            profound implications for AI and Neural Networks research and industry deployment. By welcoming diverse
+            perspectives from policy and lawmakers, industry, and academia, the workshop unveils how emerging frameworks
+            such as the European AI Act will shape the neural networks research community's technical innovation,
+            deployment, and priorities. This collaborative discussion will assist the community in navigating the complex
+            regulatory landscape, provide insights for AI system pioneers, and facilitate alignment with emerging
+            certification and standardization mechanisms and fundamental frameworks on human rights and AI.
+        </p>
+        <p>
+            Organizers:
+            <ul>
+                <li>
+                    <b>Amanda Horzyk</b>:
+                    Researcher in AI and Technology Law and Regulation, Data and AI Ethics Tutor,
+                    PhD Affiliate at Centre for Technomoral Futures, University of Edinburgh, UK 
+                </li>
+                <li>
+                    <b>Nicola Fabiano</b>:
+                    Adjunct Professor, Ostrava University (Rome), United Nations University
+                    Artificial Intelligence Network (UNU AI Network) member
+                </li>
+                <li>
+                    <b>Asim Roy</b>:
+                    Professor of Information Systems, Arizona State University.
+                </li>
+                <li>
+                    <b>Maja Nisevic</b>:
+                    Postdoctoral Researcher at KU Leuven at the Centre for IT & IP Law.
+                </li>
+            </ul>
+        </p>
+        <p>
+            Contact Area Chair(s):<br/>
+            Amanda M Horzyk <a href="mailto:amandahorzyk@outlook.com">amandahorzyk@outlook.com</a><br/>
+            Prof Asim Roy <a href="mailto:asim.roy@asu.edu">asim.roy@asu.edu</a>
+        </p>
+        <table class="border-collapse">
+            <tr class="bg-secondary-600/50">
+                <th class="border-y-2 border-x-0 p-3 border-y-secondary-600 w-1/2">
+                    Workshop Artivities
+                </th>
+                <th class="border-y-2 border-x-0 p-3 border-y-secondary-600 w-1/2">
+                    Moderators
+                </th>
+            </tr>
+            <tr>
+                <td class="border-r border-gray-400 p-3">
+                    <b>Foreword by Maria Tripodi</b>, Undersecretary for Foreign Affairs.<br/>
+                    <b>Plenary Talk by Brando Benifei</b>, Co-rapporteur for the AI Act.<br/>
+                </td>
+                <td class="border-l border-gray-400 p-3">
+                    Nicola Fabiano,<br/>
+                    Danilo C.
+                </td>
+            </tr>
+            <tr class="bg-secondary-600/5">
+                <td class="border-y border-r border-secondary-400 p-3">
+                    <b>Panel Discussion 1</b>: Impact of European and International AI Policy, Regulation on Neural Networks<br/>
+                    <br/>
+                    <i>Confirmed Panellists</i>:<br/>
+                    Alexander Kriebitz<br/>
+                    Karine Caunes<br/>
+                    Virginia Ghiara<br/>
+                    Denis Naughton<br/>
+                </td>
+                <td class="border-y border-l border-secondary-400 p-3">
+                    Amanda Horzyk<br/>
+                    Prof Asim Roy
+                </td>
+            </tr>
+            <tr class="border-b-2 border-secondary-600">
+                <td class="border-r border-gray-400 p-3">
+                    <b>Panel Discussion 2</b>: Responding to Change: Implementation, standardization and certification for Research and Development in AI<br/>
+                    <br/>
+                    <i>Confirmed Panellists</i>:<br/>
+                    Domenico Squillace<br/>
+                    Valentina Grazia Sapuppo<br/>
+                    Prof. Massimo Ruffolo<br/>
+                    Prof. Georg Gottlob<br/>
+                </td>
+                <td class="border-l border-gray-400 p-3">
+                    Nicola Fabiano
+                </td>
+            </tr>
+        </table>
     </div>
 </template>
 
